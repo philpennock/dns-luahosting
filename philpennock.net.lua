ebd948f2652abf689d8e8314d9e9f38d567f8a03
Phil Pennock <phil+git@pennock-tech.com>
2021-01-16 18:25:42 -0500

    Update records, nuke ancient demos

--- philpennock.net.lua
+++ philpennock.net.lua
@@ -4,16 +4,24 @@
 -- Variable _a is replaced with zone name
 -- _a = "example.com"
 
-local ttl = 300
+local ttl = 600
 
 local nl_services_ipv4 = '94.142.241.93'
 local nl_services_ipv6 = '2a02:898:31::48:4558:5345:5256'
 
-local subdomains_to_services = { 'www', 'cands', 'demo', 'fred' }
+local subdomains_to_services = { 'www', 'demo' }
 
 for val in values(subdomains_to_services) do
   a(val, nl_services_ipv4, ttl)
   aaaa(val, nl_services_ipv6, ttl)
 end
 
-redirect('conf', 'http://abstractions.io/')
+-- alias purely for testing
+alias('bridge', 'bridge.grumpy-troll.org')
+
+-- redirect('conf', 'http://abstractions.io/')
+
+caa('', 'letsencrypt.org', 'issue')
+caa('', 'amazonaws.com', 'issue')
+caa('', 'globnix.net', 'issue')
+caa('', ';', 'issuewild')
